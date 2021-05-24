4828baae5d1e1eae3442e10afe3a9a932a5a6cc5
Augustus <62512821+AugustusUkiyo@users.noreply.github.com>
2021-05-24 15:04:44 +0200

    Add files via upload

--- edit.py
+++ edit.py
@@ -1,6 +1,6 @@
 with open('data.csv', "r", encoding='utf-8') as fh:
     lines = fh.readlines()
-with open('new_data1.csv', "w", encoding='utf-8') as fh:
+with open('new_data.csv', "w", encoding='utf-8') as fh:
     #lines = fh.readlines()
     count = 0
     for line in lines:
@@ -10,14 +10,3 @@ with open('new_data1.csv', "w", encoding='utf-8') as fh:
         line_list = line.split(';')
         if len(line_list) == 25:
             fh.write(line)
-        """
-        count += 1
-
-        if not line_list[1].isnumeric():
-            print('no')
-        else:
-            count += 1
-        print(count)
-    #print()
-    """
-    #print(count)
